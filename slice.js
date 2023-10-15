cdc6ccf521fa4cc5ce911f725b31cb5d1aeaeca5
upadhayayansu <upadhayayansu8@gmail.com>
2023-10-15 15:14:03 +0545

    splice method

---
+++ slice.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const numbers2 = numbers.slice(3, 5);
+console.log(numbers2);
+
+//
+
+const fruits = ["apple", "banana", "mango", "orange"];
+const fruits3 = fruits.slice(0, 3);
+console.log(fruits3);
