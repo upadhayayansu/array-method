22e3f67edcae9b70589883716ff0658def73fd13
upadhayayansu <upadhayayansu8@gmail.com>
2023-10-17 09:08:59 +0545

    include

--- fill.js
+++ fill.js
@@ -1,3 +1,10 @@
 const numbers = [1, 2, 3, 4, 5];
 numbers.fill(1);
 console.log(numbers);
+
+function fillNumbers(n) {
+  return Array(n)
+    .filter(0)
+    .map((_, inx) => idx + 1);
+}
+console.log(fillInNumbers(10));
---
+++ includes.js
@@ -0,0 +1,12 @@
+const names = ["ram", "shyam", "hari"];
+const res = names.includes("arjun");
+console.log(res);
+
+//
+const fruits = ["apple", "mango"];
+
+if (res) {
+  console.log("yuppy");
+} else {
+  console.log("sad...Lets buy an orange");
+}
