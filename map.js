c92e19220d3cbb73832a9530592b3b79773fe1ad
upadhayayansu <upadhayayansu8@gmail.com>
2023-11-27 13:57:37 +0545

    unique

--- map.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-
-const numbersDouble = numbers.map(double);
-function double(value, index, arr) 
-console.log(numbersDouble);
---
+++ unique element.js
@@ -0,0 +1,2 @@
+const arr = [1, 1, 1, 1, 2, 2, 3];
+console.log(Array.from(new Set(arr)));
